75d5140f415264aad98a1ef90c807fe9e0910844
Rocio <roaguerrero17@gmail.com>
2022-06-24 01:25:35 -0300

    agregado dataTableEmpleado, faltan estilos

---
+++ src/app/services/dependencia.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Dependencia } from '../models/dependencia';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DependenciaService {
+
+  URL: string;
+
+  constructor(private _http: HttpClient) { 
+    this.URL = "http://localhost:8000/api/v1/dependencias";
+  }
+
+  public addEDependencia(dependencia: Dependencia): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const body = JSON.stringify(dependencia);
+    return this._http.post(this.URL, body, options);
+  }
+
+  public getDependencias(): Observable<any> {
+    const options = {headers: new HttpHeaders({})};
+    return this._http.get(this.URL, options);
+  }
+}
---
+++ src/app/services/empleado.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Empleado } from '../models/empleado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+
+  URL: string;
+
+  constructor(private _http: HttpClient) { 
+    this.URL = "http://localhost:8000/api/v1/empleados";
+  }
+
+  public addEmpleado(empleado: Empleado): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const body = JSON.stringify(empleado);
+    return this._http.post(this.URL, body, options);
+  }
+
+  public getEmpleados(): Observable<any> {
+    const options = {headers: new HttpHeaders({})};
+    return this._http.get(this.URL+ "/" , options);
+  }
+
+  public updateEmpleado(empleado: Empleado): Observable<any> {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const body = JSON.stringify(empleado);
+    return this._http.put(this.URL + "/" + empleado._id, body, options);
+  }
+
+  public deleteEmpleado(id: string): Observable<any> {
+    const options = {headers: new HttpHeaders({})};
+    return this._http.delete(this.URL + "/" + id, options);
+  }
+}
