b2f7390e8133e7ecb368f40b08eca81c7af5a9c6
Rocio <roaguerrero17@gmail.com>
2022-07-08 01:01:09 -0300

    graf barras oficina

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartData, ChartDataset, ChartEvent, ChartType } from 'chart.js';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { OficinaService } from 'src/app/services/oficina.service';
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
@@ -7,43 +7,63 @@ import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 @Component({
   selector: 'app-estadisticas',
   templateUrl: './estadisticas.component.html',
-  styleUrls: ['./estadisticas.component.css']
+  styleUrls: ['./estadisticas.component.css'],
 })
 export class EstadisticasComponent implements OnInit {
-
-  constructor(private oficinaService: OficinaService) { 
-  }
-  oficinas:Array<any>=[];
-  valores:Array<any>=[];
-  date=new Date()
-  dates:Array<string>=[]
-  range=3
+  constructor(private oficinaService: OficinaService) {}
+  oficinas: Array<any> = [];
+  valores: Array<any> = [];
+  data:Array<any> =[]
+  fechas = ['2022-07', '2022-08', '2022-09']
   ngOnInit(): void {
     this.getEstadisticasOficinas();
   }
-  getEstadisticasOficinas(){
+  getEstadisticasOficinas() {
     this.oficinaService.getEstadisticasOficinas().subscribe({
-      next:(result)=>{
-        this.oficinas=[];
-        this.oficinas=result['data']['oficinas'];
-        console.log(this.oficinas);
-        this.generateValues()
-        this.update()
+      next: (result) => {
+        this.oficinas = [];
+        this.oficinas = result['data']['oficinas'];
+        this.filterValues();
+        this.getLabelsBarOficina()
+        this.update();
       },
       error: () => {
         alert('Error en la peticion');
       },
-    })
+    });
   }
-  generateValues(){
-    this.oficinas.forEach(x=>this.valores.push({data:Object.values(x["historialDeReuniones"]), label:x["nombre"]}))
-    console.log(this.valores)
+  filterValues() {
+    this.valores = this.oficinas.map((x: any) => ({
+      nombre: x['nombre'],
+      historial: x['historialDeReuniones'].map((y: any) => ({
+        fecha: y['horaInicio'].slice(0, 7),
+        participantes: y['participantes'].length,
+      })),
+    }));
+    this.fechas.forEach((i) =>
+      this.valores.forEach((obj: any) => {
+        obj[i] = {
+          usos: obj['historial'].filter((x: any) => x.fecha == i).length,
+          participantes: obj['historial']
+            .filter((x: any) => x.fecha == i)
+            .reduce((count: number, current: any) => {
+              count = count + current['participantes'];
+              return count;
+            }, 0),
+        };
+      })
+    );
+    this.valores.forEach((obj: any) => delete obj['historial']);
   }
-  generateDates(){
-    for(let i=0;i<this.range;i++){
-      this.dates.push(this.date.getFullYear() + "-" +(this.date.getMonth()+i))
-    }
+  getLabelsBarOficina(){
+    let array:Array<any> =[]
+    this.valores.forEach((x:any)=>{ let arr:Array<any>=[]; this.fechas.forEach(i => {
+        arr.push(x[i].usos)});
+        array.push(arr);
+        })
+    this.valores.forEach((x:any, index:number)=>(this.data.push({label:x["nombre"], data:array[index]})))
   }
+
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   public barChartOptions: ChartConfiguration['options'] = {
@@ -53,8 +73,8 @@ export class EstadisticasComponent implements OnInit {
       x: {},
       y: {
         min: 0,
-        max:10
-      }
+        max: 10,
+      },
     },
     plugins: {
       legend: {
@@ -62,34 +82,104 @@ export class EstadisticasComponent implements OnInit {
       },
       datalabels: {
         anchor: 'end',
-        align: 'end'
-      }
-    }
+        align: 'end',
+      },
+    },
   };
   public barChartType: ChartType = 'bar';
-  public barChartPlugins = [
-    DataLabelsPlugin
-  ];
+  public barChartPlugins = [DataLabelsPlugin];
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '2022-07', '2022-08', '2022-09' ],
-    datasets: this.valores
+    labels: this.fechas,
+    datasets: this.data,
   };
 
   // events
-  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
     console.log(event, active);
   }
 
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
     console.log(event, active);
   }
 
   public update(): void {
-    // Only Change 3 values
-    this.valores=[]
-    this.generateValues()
+
+    this.valores = [];
+    this.filterValues();
 
     this.chart?.update();
   }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  //ESTADISTICA TIPO TORTA
+
+  public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+      datalabels: {
+        formatter: (value, ctx) => {
+          if (ctx.chart.data.labels) {
+            return ctx.chart.data.labels[ctx.dataIndex];
+          }
+        },
+      },
+    },
+  };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: ['2022-07', '2022-08', '2022-09'],
+    datasets: this.valores,
+  };
+  public pieChartType: ChartType = 'pie';
+  public pieChartPlugins = [DataLabelsPlugin];
+
+  changeLegendPosition(): void {
+    if (this.pieChartOptions?.plugins?.legend) {
+      this.pieChartOptions.plugins.legend.position =
+        this.pieChartOptions.plugins.legend.position === 'left'
+          ? 'top'
+          : 'left';
+    }
+
+    this.chart?.render();
+  }
+
+  toggleLegend(): void {
+    if (this.pieChartOptions?.plugins?.legend) {
+      this.pieChartOptions.plugins.legend.display =
+        !this.pieChartOptions.plugins.legend.display;
+    }
+
+    this.chart?.render();
+  }
 }
+
+
